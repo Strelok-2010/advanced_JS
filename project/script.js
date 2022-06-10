d6769549df53fddcc4e5aa0324fda0f022ebc5c8
Strelok-2010 <karpushin.Vladimir.86@mail.ru>
2022-06-10 13:14:34 +0300

    lesson-6

--- project/script.js
+++ project/script.js
@@ -11,6 +11,55 @@ function service(url) {
 let app = null
 
 window.onload = () => {
+	Vue.component('custom_input', {
+		model: {
+			prop: 'entry',
+			event: 'change'
+		},
+		props: {
+			entry: String
+		},
+		template: `
+		<input
+			type="text" 
+			class="goods-search"
+			v-bind:entry="entry"
+			v-on:change="$emit('change', $event.target.value)"
+		/>
+		`
+	})
+	Vue.component('custom_button', {
+		template: `
+		<button class="cart-button" type="button" @click="$emit('click')">
+			<slot></slot>
+		</button>
+		`
+	})
+	Vue.component('good', {
+		props: [
+			'item'
+		],
+		template: `
+			<div class="goods-item">
+				<h3>{{ item.product_name }}</h3>
+				<p>{{ item.price }}</p>
+			</div>
+			`
+	})
+	Vue.component('snackbar', {
+		props: [
+			'error',
+			'snack'
+		],
+		template: `
+		<div class="bug">
+			<div class="error">{{error}}</div>
+			<custom_button @click="snack">Close</custom_button>
+		</div>
+			`
+	})
+
+
 
 	app = new Vue({
 		el: '#root',
@@ -19,7 +68,8 @@ window.onload = () => {
 			itemsCarts: [],
 			searchValue: '',
 			isVisibleCart: false,
-			carts: []
+			snackbar: false,
+			errorMessage: ''
 		},
 		methods: {
 			fetchGoods() {
@@ -39,6 +89,15 @@ window.onload = () => {
 				else {
 					this.isVisibleCart = true;
 				}
+			},
+			visiblBug() {
+				this.snackbar = !this.snackbar
+			},
+			timeVisiblBug() {
+				this.snackbar = !this.snackbar;
+				setTimeout(() => {
+					this.snackbar = false;
+				}, "3000")
 			}
 		},
 		computed: {
@@ -56,6 +115,22 @@ window.onload = () => {
 		mounted() {
 			this.fetchGoods();
 			this.fetchCarts()
+		},
+
+		created() {
+			fetch("https://api.npms.io/v2/invalid-url")
+				.then(async response => {
+					const data = await response.json();
+					if (!response.ok) {
+						const error = (data && data.message + response.status) || response.statusText;
+						return Promise.reject(error);
+					}
+					this.totalVuePackages = data.total;
+				})
+				.catch(error => {
+					this.errorMessage = error;
+					this.timeVisiblBug();
+				});
 		}
 	})
 }
